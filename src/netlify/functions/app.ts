19eef9c00ada1f36011c5f516008b86fef54ecee
Russell Warlow <52016758+russell-warlow@users.noreply.github.com>
2024-02-08 19:44:32 -0800

    moving all typescript files into root directory

--- src/netlify/functions/app.ts
+++ src/netlify/functions/app.ts
@@ -4,10 +4,10 @@ import path from "path";
 
 const app: Application = express();
 
-app.set("views", path.join(__dirname, "..", "..", "public", "views"));
+app.set("views", path.join(__dirname, "..", "..", "..", "public", "views"));
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, "..", "..", "public")));
+app.use(express.static(path.join(__dirname, "..", "..", "..", "public")));
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.render("index");
